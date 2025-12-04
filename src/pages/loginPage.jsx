cd181a4625a4fd33e1f7b0ee72de21a84b38eade
Jonny12341234641 <kavinduuni@gmail.com>
2025-12-04 23:24:17 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,12 +1,7 @@
 export default function LoginPage() {
     return (
-        <div className="w-full h-full bg-primary flex flex-row p-2">
-            <div className="w-[300px] h-full bg-primary">
-                
+            <div className="w-full h-full bg-[url(/src/assets/login.jpg)] bg-cover bg-center bg-no-repeat">
+                <h1>Login Page</h1>
             </div>
-            <div className="w-[calc(100%-300px)] h-full border-2 border-accent rounded-[20px]">
-                <h1>Login</h1>
-            </div>
-        </div>
     )
 }
