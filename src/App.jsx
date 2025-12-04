9c3b4925f9ccb24ef0d1542745a4c08324efe110
Jonny12341234641 <kavinduuni@gmail.com>
2025-12-04 23:01:48 +0530

    Add LoginPage component and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdminPage from './pages/adminPage.jsx';
 import HomePage from './pages/homePage.jsx';
 import TestPage from './pages/test.jsx';
+import LoginPage from './pages/loginPage.jsx';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/register" element={<h1>Register Page</h1>}></Route>
                     <Route path="/admin/*" element={<AdminPage/>}></Route> //* means any path inside admin page can be accessed either by /admin or /admin/anything
                     <Route path="/test" element={<TestPage/>}></Route>
+                    <Route path="/login" element={<LoginPage/>}></Route>
                 </Routes>
             </div>
         </BrowserRouter>
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+    return (
+        <div className="w-full h-full bg-primary flex flex-row p-2">
+            <div className="w-[300px] h-full bg-primary">
+                
+            </div>
+            <div className="w-[calc(100%-300px)] h-full border-2 border-accent rounded-[20px]">
+                <h1>Login</h1>
+            </div>
+        </div>
+    )
+}
