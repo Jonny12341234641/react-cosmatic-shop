ea5db47955fdd189287597110b8fb8741604ef3b
Jonny12341234641 <kavinduuni@gmail.com>
2025-12-05 11:51:14 +0530

    feat: enhance AdminPage layout with navigation links and icons for better user experience

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,14 +1,36 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import { FaChartLine } from "react-icons/fa";
+import { BsCartPlusFill } from "react-icons/bs";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { AiOutlineProduct } from "react-icons/ai";
+
+
 
 export default function AdminPage() {
     return (
         <>
             <div className="w-full h-full bg-primary flex flex-row p-2 text-secondary">
-                <div className="w-[300px] h-full bg-primary flex flex-col text-white items-center gap-[20px]">
-                    <div className="flex flex-row w-[90%] h-[70px] bg-accent items-center rounded-xl">
+                <div className="w-[300px] h-full bg-primary flex flex-col text-white items-center gap-[10px]">
+                    <div className="flex flex-row w-[90%] h-[70px] bg-accent items-center rounded-xl mb-[20px]">
                         <img className="h-[95px] object-cover" src="/logo.png" alt="" />
                         <span className="text-white text-lg ml-4">Admin Panel</span>
                     </div>
+                    <Link to="/admin" className="w-[90%] text-secondary flex items-center gap-2 px-4 hover:bg-accent rounded-lg">
+                        <FaChartLine className="text-secondary text-2xl" />
+                        Dashboard
+                    </Link>
+                    <Link to="/admin/orders" className="w-[90%] text-secondary flex items-center gap-2 px-4 hover:bg-accent rounded-lg">
+                        <BsCartPlusFill className="text-secondary text-2xl" />
+                        Orders
+                    </Link>
+                    <Link to="/admin/products" className="w-[90%] text-secondary flex items-center gap-2 px-4 hover:bg-accent rounded-lg">
+                        <AiOutlineProduct className="text-secondary text-2xl" />
+                        Products
+                    </Link>
+                    <Link to="/admin/users" className="w-[90%] text-secondary flex items-center gap-2 px-4 hover:bg-accent rounded-lg">
+                        <HiOutlineUsers className="text-secondary text-2xl" />
+                        Users
+                    </Link>
                 </div>
                 <div className="w-[calc(100%-300px)] h-full border-[4px] border-accent rounded-[20px] overflow-hidden shadow">
                     <div className="h-full width-full max-width-full max-h-full overflow-y-scroll">
